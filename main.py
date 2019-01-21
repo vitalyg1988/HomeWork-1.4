6f3e7decfcd9be5e7162556c5960f26b072eee39
Vitaly Golubev <vitalyg1988@gmail.com>
2019-01-21 14:48:34 +0400

    Add cycle FOR task1

--- main.py
+++ main.py
@@ -7,21 +7,37 @@ with open('output.csv', newline='') as csvfile:
 	flats_list = list(flats_csv)
 
 #можете посмотреть содержимое файла с квартирами через print, можете - на вкладке output.csv
-#print (flats_list)
+# print (flats_list)
 
 
 #TODO 1:
 # 1) Напишите цикл, который проходит по всем квартирам, и показывает только новостройки
 #и их порядковые номера в файле. Подсказка - вам нужно изменить этот код:
-for flat in flats_list:
-  if "новостройка" in flat:
-    print("{}".format(flat))
+print("TASK 1:")
+print(' ')
+num_id = []
+new_flats = []
+for index, flat in enumerate(flats_list):
+  if flat[2] == "новостройка":
+    new_flats.append(flat)
+    num_id.append(flat[0])
+  print("Квартиры новостройки: {} из {}".format(index, new_flats))
+print(' ')
+print("Количество новостроек: ", len(num_id))
+print(' ')
+print("Список id-новостроек: ", num_id)
+
 # 2) добавьте в код подсчет количества новостроек
 
 
 #TODO 2:
 # 1) Сделайте описание квартиры в виде словаря, а не списка. Используйте следующие поля из файла output.csv: ID, Количество комнат;Новостройка/вторичка, Цена (руб). У вас должно получиться примерно так:
-flat_info = {"id":flat[0], "rooms":flat[1], "type":flat[2], "price":flat[11]}
+print("TASK 2:")
+print(' ')
+flat_info = {}
+# for flat in flats_list:
+
+# flat_info = {"id":flat[0], "rooms":flat[1], "type":flat[2], "price":flat[11]}
 # 2) Измените код, который создавал словарь для поиска квартир по метро так, чтобы значением словаря был не список ID квартир, а список описаний квартир в виде словаря, который вы сделали в п.1
 subway_dict = {}
 for flat in flats_list:
@@ -29,6 +45,6 @@ for flat in flats_list:
   if subway not in subway_dict.keys():
     subway_dict[subway] = list()
   subway_dict[subway].append(flat[0])
-print(subway_dict)
+#print(subway_dict)
 
 # 3) Самостоятельно напишите код, который подсчитывает и выводит, сколько квартир нашлось у каждого метро.
