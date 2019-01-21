7255a3835d18c8e3b15de732bc5b547fb9435a42
Vitaly Golubev <vitalyg1988@gmail.com>
2019-01-21 17:32:31 +0400

    add cycles, Doing task1 task2

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ with open('output.csv', newline='') as csvfile:
 	flats_csv = csv.reader(csvfile, delimiter=';')
 	flats_list = list(flats_csv)
 
+header = flats_list.pop(0)
 #можете посмотреть содержимое файла с квартирами через print, можете - на вкладке output.csv
 # print (flats_list)
 
@@ -16,12 +17,10 @@ with open('output.csv', newline='') as csvfile:
 print("TASK 1:")
 print(' ')
 num_id = []
-new_flats = []
-for index, flat in enumerate(flats_list):
+for flat in flats_list:
   if flat[2] == "новостройка":
-    new_flats.append(flat)
     num_id.append(flat[0])
-  print("Квартиры новостройки: {} из {}".format(index, new_flats))
+    print("Квартира новостройка: {} ".format(flat))
 print(' ')
 print("Количество новостроек: ", len(num_id))
 print(' ')
@@ -32,19 +31,26 @@ print("Список id-новостроек: ", num_id)
 
 #TODO 2:
 # 1) Сделайте описание квартиры в виде словаря, а не списка. Используйте следующие поля из файла output.csv: ID, Количество комнат;Новостройка/вторичка, Цена (руб). У вас должно получиться примерно так:
+print(' ')
 print("TASK 2:")
 print(' ')
 flat_info = {}
-# for flat in flats_list:
-
-# flat_info = {"id":flat[0], "rooms":flat[1], "type":flat[2], "price":flat[11]}
+for flat in flats_list:
+  flat_info = {"ID":flat[0], "Количество комнат":flat[1], "Новостройка/вторичка":flat[2], "Цена(руб.)":flat[11]}
+  print(flat_info)
 # 2) Измените код, который создавал словарь для поиска квартир по метро так, чтобы значением словаря был не список ID квартир, а список описаний квартир в виде словаря, который вы сделали в п.1
+print(' ')
 subway_dict = {}
 for flat in flats_list:
   subway = flat[3].replace("м.", "")
-  if subway not in subway_dict.keys():
-    subway_dict[subway] = list()
-  subway_dict[subway].append(flat[0])
-#print(subway_dict)
+  # if subway not in subway_dict.keys():
+  #   subway_dict[subway] = list()
+  subway_dict.setdefault(subway, list())
+  subway_dict[subway].append({flat[12]})
+
+print(subway_dict)
 
 # 3) Самостоятельно напишите код, который подсчитывает и выводит, сколько квартир нашлось у каждого метро.
+print(' ')
+for subway, flat_info in subway_dict.items():
+  print('Метро {0} количество квартир: {1}'.format(subway, len(flat_info)))
